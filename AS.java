33c462c5e74c8945809b1fb34192591b929e56be
BaptisteDeslaurier <baptiste.deslaurier@bts-malraux.net>
2014-10-09 23:12:35 +0200

    Version 4

---
+++ AS.java
@@ -0,0 +1,33 @@
+package remboursement;
+
+/**
+ * AS est la classe comprenant toutes les données pour un aller simple.
+ *
+ * @author BaptisteDeslaurier
+ * @version 1.0
+ */
+
+public class AS extends Tarif{
+
+	//*************   ATTRIBUTS    *************
+	private double tarifASJS;
+	private double tarifASNW;
+
+	//*************   ACCESSEURS    *************
+	public double getTarifASJS()
+	{
+	    return tarifASJS;
+	}
+
+	public double getTarifASNW()
+	{
+	    return tarifASNW;
+	}
+
+	//*************   CONSTRUCTEUR   *************
+	AS(int dept, double priseEnCharge, double horaireJS, double horaireNW, double tarifASJS, double tarifASNW){
+		super(dept, priseEnCharge, horaireJS, horaireNW);
+		this.tarifASJS = tarifASJS;
+		this.tarifASNW = tarifASNW;
+	}
+}
