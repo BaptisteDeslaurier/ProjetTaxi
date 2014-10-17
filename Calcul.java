764dbfdf3fe23a37ca8f880384caff530d95c860
TristanSebillet <tristan.sebillet@bts-malraux.net>
2014-10-17 09:10:00 +0000

    Premier commit de la version 5

---
+++ Calcul.java
@@ -0,0 +1,66 @@
+package rembWBP;
+import java.util.List;
+
+/**
+ * Calcul est la classe comprenant le calcul du remboursement.
+ *
+ * @author BaptisteDeslaurier
+ * @version 1.0
+ */
+
+public class Calcul {
+
+	/**
+     * Retourne le montant du remboursement.
+     *
+     * @return un décimal montantRemb, qui correspond au montant du remboursement.
+     */
+
+	public static double calculer(int i, List<AR> maListeAR, List<AS> maListeAS , boolean rdbtnAr, boolean rdbtnAs, boolean rdbtnJ, boolean rdbtnN, boolean rdbtnS, boolean rdbtnWe, int txtTemps, int txtKm)
+	{
+		double montantRemb = 0;
+
+		//Calcul remboursement
+				//Si c'est un aller simple
+				if(rdbtnAs){
+					//Si c'est en semaine de jour
+					if(rdbtnS && rdbtnJ){
+						montantRemb = maListeAS.get(i).getPriseEnCharge() + (txtKm * maListeAS.get(i).getTarifASJS());
+
+						//Si le trajet dépasse 1h
+						if(txtTemps > 1){
+							montantRemb += montantRemb + (txtTemps * maListeAS.get(i).getHoraireJS());
+						}
+					}else
+					//Sinon c'est de semaine de nuit ou en WE
+					if((rdbtnS && rdbtnN) || rdbtnWe){
+						montantRemb = maListeAS.get(i).getPriseEnCharge() + (txtKm * maListeAS.get(i).getTarifASNW());
+
+						//Si le trajet dépasse 1h
+						if(txtTemps > 1){
+							montantRemb += montantRemb + (txtTemps * maListeAS.get(i).getHoraireNW());
+						}
+					}
+				}else
+					//Si c'est en semaine de jour
+					if(rdbtnS && rdbtnJ){
+						montantRemb = maListeAR.get(i).getPriseEnCharge() + (txtKm * maListeAR.get(i).getTarifARJS());
+
+						//Si le trajet dépasse 1h
+						if(txtTemps > 1){
+							montantRemb += montantRemb + (txtTemps * maListeAR.get(i).getHoraireJS());
+						}
+					}else
+					//Sinon c'est de semaine de nuit ou en WE
+					if((rdbtnS && rdbtnN) || rdbtnWe){
+						montantRemb = maListeAR.get(i).getPriseEnCharge() + (txtKm * maListeAR.get(i).getTarifARNW());
+
+						//Si le trajet dépasse 1h
+						if(txtTemps > 1){
+							montantRemb += montantRemb + (txtTemps * maListeAR.get(i).getHoraireNW());
+						}
+				}
+
+				return montantRemb;
+	}
+}
---
+++ Tarif.java
@@ -0,0 +1,51 @@
+package rembWBP;
+
+/**
+ * Tarif est la classe comprenant toutes les données pour un déplacement sans faire la distinction entre aller simple ou aller retour.
+ *
+ * @author BaptisteDeslaurier
+ * @version 1.0
+ */
+
+public class Tarif {
+
+	private int dept;
+	private double priseEnCharge;
+	private double horaireJS;
+	private double horaireNW;
+
+	//*************   ACCESSEURS    *************
+	/**
+     * Accesseur du numéro du département.
+     *
+     * @return Une instance de SDZLevel, qui correspond à niveau du membre sur SDZ.
+     */
+	public int getDept()
+	{
+	    return dept;
+	}
+
+	public double getPriseEnCharge()
+	{
+	    return priseEnCharge;
+	}
+
+	public double getHoraireJS()
+	{
+	    return horaireJS;
+	}
+
+	public double getHoraireNW()
+	{
+	    return horaireNW;
+	}
+
+
+	//*************   CONSTRUCTEUR   *************
+	Tarif(int dept, double priseEnCharge, double horaireJS, double horaireNW){
+		this.dept = dept;
+		this.priseEnCharge = priseEnCharge;
+		this.horaireJS = horaireJS;
+		this.horaireNW = horaireNW;
+	}
+}
